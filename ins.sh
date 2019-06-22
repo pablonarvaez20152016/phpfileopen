1c5effce1a8a4e226917b5133a7bc0062fd58487
pablo <pablonarvaez20152016@gmail.com>
2019-06-22 18:20:27 -0500

    ins.sh modificado

--- ins.sh
+++ ins.sh
@@ -5,17 +5,17 @@ cp index.php /opt/lampp/htdocs
 cp login.php /opt/lampp/htdocs
 cp procesar.php /opt/lampp/htdocs
 cp procesarregistro.php /opt/lampp/htdocs
-cp style.css /opt/lampp/htdocs
+cp styles.css /opt/lampp/htdocs
 cp udenar.png /opt/lampp/htdocs
 cp clave.txt /opt/lampp/htdocs
-sudo chmod 777 /opt/lammp/htdocs/login.php
-sudo chmod 777 /opt/lammp/htdocs/procesar.php
-sudo chmod 777 /opt/lammp/htdocs/index.php
-sudo chmod 777 /opt/lammp/htdocs/procesarregistro.php
-sudo chmod 777 /opt/lammp/htdocs/style.php
-sudo chmod 777 /opt/lammp/htdocs/clave.txt
-sudo chmod 777 /opt/lammp/htdocs
-/opt/lammp/lammp start
+sudo chmod 777 /opt/lampp/htdocs/login.php 
+sudo chmod 777 /opt/lampp/htdocs/procesar.php
+sudo chmod 777 /opt/lampp/htdocs/index.php
+sudo chmod 777 /opt/lampp/htdocs/procesarregistro.php
+sudo chmod 777 /opt/lampp/htdocs/styles.css
+sudo chmod 777 /opt/lampp/htdocs/clave.txt
+sudo chmod 777 /opt/lampp/htdocs
+/opt/lampp/lampp start
 
 
 
